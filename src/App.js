aa919e73557cae461577725f8950d95ea887cab3
Rajat Verma <rajatvermag@gmail.com>
2019-09-08 13:00:01 +0530

    updating the app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 import './pages/homepage.styles.scss';
 
 import HomePage from '../src/pages/homepage.component';
-
+import ShopPage from './pages/shop/shop.component';
+// eslint-disable-next-line
 const HatsPage = () => (
   <div>
     <h1>Hats Page</h1>
@@ -16,7 +17,7 @@ function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/hats' component={HatsPage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
